3de3453c31301838203644a3b1f967d71330058c
Frazer Findlater <frazer.findlater@mirumagecny.com>
2018-11-05 17:41:32 +0000

    More mobile tweaks

--- src/components/header.js
+++ src/components/header.js
@@ -25,10 +25,8 @@ class Header extends React.Component {
               </ul>
               <div id="outer-container">
                 <Menu right outerContainerId={ "outer-container" }>
-                  <a id="home" className="menu-item" href="/">Home</a>
-                  <a id="about" className="menu-item" href="/about">About</a>
-                  <a id="contact" className="menu-item" href="/contact">Contact</a>
-                  <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+                  <Link to={`/`}>Home</Link>
+                  <Link to={`/blog/`}>Blog</Link>
                 </Menu>
               </div>
             </div>
